16bcf114fa5ac5d8eda96a8b0f50bc70e54fa39e
tonylampada <tonylampada@gmail.com>
2012-09-25 14:31:47 -0300

    fix travis

---
+++ runTravisRUN.sh
@@ -0,0 +1,6 @@
+export PATH=../tmp/bin:$PATH; 
+echo '------------- RUNNING TESTS... ----------------'; 
+code=$(./manage.py test core); 
+echo '------------- TESTS EXECUTION FINISHED (logs below) ----------------'; 
+cat logs/frespo.log; 
+exit $code
