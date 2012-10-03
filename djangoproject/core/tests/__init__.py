26d3e4c9a3c6ced6a509f6f2f39647f03360f06e
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2012-10-02 22:03:00 -0300

    ISSUE-16 ISSUE-57 using coverage to procude coverage reports (can be adapted to produce jenkins-compatible output later) and created unit tests for model entities. The same pattern can be followed for other tests

--- djangoproject/core/tests/__init__.py
+++ djangoproject/core/tests/__init__.py
@@ -3,5 +3,6 @@ from tests_trackerintegration import *
 from splintertests_users import *
 from splintertests_comments import *
 from splintertests_payment import *
+from core.tests.test_model_entities import *
 
 __author__ = 'tony'
