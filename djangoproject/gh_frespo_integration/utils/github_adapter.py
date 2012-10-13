5b76aaf19c447f437e385d1f19b2a9bd58657039
tonylampada <tonylampada@gmail.com>
2012-10-13 16:14:27 -0300

    #31 Adding adapter function to create bot comments.

--- djangoproject/gh_frespo_integration/utils/github_adapter.py
+++ djangoproject/gh_frespo_integration/utils/github_adapter.py
@@ -3,7 +3,10 @@ from urlparse import urlparse
 import re
 from xml.dom.minidom import parseString
 import json
+import requests
+from django.conf import settings
 
+BOT_AUTH = (settings.GITHUB_BOT_USERNAME, settings.GITHUB_BOT_PASSWORD)
 
 def _fetch_json_objects_from_url(h, url):
     resp, content = h.request(url)
@@ -24,3 +27,10 @@ def fetch_repos(username):
         org_repos = _fetch_json_objects_from_url(h,"https://api.github.com/orgs/%s/repos"%org['login'])
         repos.extend(org_repos)
     return repos
+
+def bot_comment(username, repo_name, issue_number, body):
+    github_url = "https://api.github.com/repos/%s/%s/issues/%s/comments"%(username, repo_name, issue_number)
+    data = json.dumps({"body": body})
+    r = requests.post(github_url, data, auth=BOT_AUTH)
+    if(r.status_code != 201):
+        raise BaseException("status %s: %s" % (r.status_code, github_url))
