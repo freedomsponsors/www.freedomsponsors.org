c0c0fd301cf95b44e9380be7e9b967195729de6e
tonylampada <tonylampada@gmail.com>
2012-12-13 11:27:54 -0200

    last-minute bugfix: payments not working

--- djangoproject/core/services/payment_services.py
+++ djangoproject/core/services/payment_services.py
@@ -33,7 +33,7 @@ def generate_payment(offer_id, receiver_count, dict, user):
             if(pay > 0):
                 solution = Solution.objects.get(pk=int(dict['solutionId_%s' % i]))
                 realPay = Decimal(pay * Decimal(1 - settings.FS_FEE))
-                part = PaymentPart.newPart(payment, solution.programmer, pay, realPay)
+                part = PaymentPart.newPart(payment, solution, pay, realPay)
                 parts.append(part)
                 sum += pay
                 realSum += realPay
