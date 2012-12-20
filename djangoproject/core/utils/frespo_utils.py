738fbf648b3dafff19fd00a2865145cb034af2ef
tony.franca <tonylampada@gmail.com>
2012-12-19 23:35:20 -0200

    Bugfix: random breakages because of user's invalid email address

--- djangoproject/core/utils/frespo_utils.py
+++ djangoproject/core/utils/frespo_utils.py
@@ -1,6 +1,11 @@
 from urlparse import urlparse
 from decimal import Decimal
 from django.core.mail import EmailMultiAlternatives
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+import logging
+
+logger = logging.getLogger(__name__)
 
 _TWOPLACES = Decimal(10) ** -2
 
@@ -53,6 +58,12 @@ def dictOrEmpty(dict, key):
     return ''
 
 def send_html_mail(subject, body_txt, body_html, from_email, to_addresses):
+    try:
+        for to_addr in to_addresses:
+            validate_email(to_addr)
+    except ValidationError:
+        logger.warn('Email not sent. Invalid email address in %s. subject = %s' % (to_addresses, subject))
+        return
     msg = EmailMultiAlternatives(subject, body_txt, from_email, to_addresses)
     msg.attach_alternative(body_html, "text/html")
     msg.send()
