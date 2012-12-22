d6d52254d59c3ccef6c879a51e03c4a1633c3d78
Henrique Bastos <henrique@bastos.net>
2012-12-22 02:52:21 -0200

    Add functional test to the view `stats`

--- djangoproject/core/tests/test_stats.py
+++ djangoproject/core/tests/test_stats.py
@@ -1,1 +1,17 @@
 # coding: utf-8
+from django.test import TestCase
+
+
+class StatsView(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/core/stats/')
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/stats.html')
+
+    def test_context(self):
+        self.assertIn('stats', self.resp.context)
+
