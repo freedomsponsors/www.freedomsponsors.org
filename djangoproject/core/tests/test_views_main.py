f10b4735de3b5f399af3076d467c5e579e045711
Henrique Bastos <henrique@bastos.net>
2012-12-30 18:26:31 -0200

    Create a HomeView testcase

--- djangoproject/core/tests/test_views_main.py
+++ djangoproject/core/tests/test_views_main.py
@@ -1,1 +1,18 @@
 # coding: utf-8
+from django.test import TestCase
+
+
+class HomeView(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        self.assertEquals(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/home.html')
+
+    def test_context(self):
+        self.assertIn('issues_sponsoring', self.resp.context)
+        self.assertIn('issues_kickstarting', self.resp.context)
+
