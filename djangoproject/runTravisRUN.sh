3ec7de49c80586046e5e4d3e9dbffe3cf73be63b
tonylampada <tonylampada@gmail.com>
2013-01-29 09:55:12 -0200

    fix travis script

--- djangoproject/runTravisRUN.sh
+++
@@ -1,7 +0,0 @@
-export PATH=../tmp/bin:$PATH
-echo '------------- RUNNING TESTS... ----------------'
-./test_all.sh
-code=$?
-echo '------------- TESTS EXECUTION FINISHED (logs below) ----------------'
-# cat logs/frespo.log
-exit $code
