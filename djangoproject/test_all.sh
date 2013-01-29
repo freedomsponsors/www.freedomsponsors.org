de38096ea8cb9bf7ccb8dfb35027fc96b8d6ac3b
tonylampada <tonylampada@gmail.com>
2013-01-29 00:54:31 -0200

    simplify travis test script

--- djangoproject/test_all.sh
+++ djangoproject/test_all.sh
@@ -1,8 +1,1 @@
-export TEST_DBMS=SQLITE
-./manage.py test core gh_frespo_integration
-code1=$?
-export TEST_DBMS=POSTGRES
-./manage.py test core_splinter_tests
-code2=$?
-code=$(expr $code1 + $code2)
-exit $code
+./manage.py test core gh_frespo_integration core_splinter_tests
