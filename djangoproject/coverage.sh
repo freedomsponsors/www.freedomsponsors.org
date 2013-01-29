6a43d9be7567285c2c758253d80c8d9ae654de7e
tonylampada <tonylampada@gmail.com>
2013-01-29 08:55:35 -0200

    runninv tests with coverage

--- djangoproject/coverage.sh
+++ djangoproject/coverage.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Coverage utility script
-
+set -e
 coverage -e
-coverage -x manage.py test core gh_frespo_integration #--settings=frespo.test_settings
+coverage -x manage.py test core gh_frespo_integration core_splinter_tests
 coverage -r -m > report.xml
 rm -Rf coverage_html_report
 coverage html '--include=core/*' '--omit=core/migrations/*'
--- djangoproject/coverageSplinter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Coverage utility script
-
-export COVERAGE_PROCESS_START=.coveragerc
-coverage -e
-coverage -x manage.py test core_splinter_tests
-coverage -r -m > report.xml
-rm -Rf coverage_html_report
-coverage html '--include=core/*' '--omit=core/migrations/*'
