b089ba1aad8d47d1901b67022b121e1413c37e26
tonylampada <tonylampada@gmail.com>
2013-01-30 00:44:50 -0200

    testing Travis push to gh-pages - the mission

--- djangoproject/upload_travis_reports.sh
+++ djangoproject/upload_travis_reports.sh
@@ -10,7 +10,7 @@ base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-git clone --quiet --branch=gh-pages git://github.com/freedomsponsors/www.freedomsponsors.org.git gh-pages
+git clone --quiet --branch=gh-pages git@github.com:freedomsponsors/www.freedomsponsors.org.git gh-pages
 cp -Rf coverage_html_report/ gh-pages/
 cd gh-pages/
 git add .
