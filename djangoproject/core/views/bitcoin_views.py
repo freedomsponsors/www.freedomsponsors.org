6994e02c1a0087ead8abfbb378611401db3f2ad9
tonylampada <tonylampada@gmail.com>
2013-02-04 19:13:27 -0200

    testing bitcoin callback

--- djangoproject/core/views/bitcoin_views.py
+++ djangoproject/core/views/bitcoin_views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
+from core.utils.frespo_utils import get_or_none
 
 def bitcoinIPN(request):
     print('----- bitcoinIPN ------')
-    print("value: %s" % request.GET["value"])
-    print("input_address: %s" % request.GET["input_address"])
-    print("confirmations: %s" % request.GET["confirmations"])
-    print("transaction_hash: %s" % request.GET["transaction_hash"])
-    print("destination_address: %s" % request.GET["destination_address"])
-    print("input_transaction_hash: %s" % request.GET["input_transaction_hash"])
+    print("value: %s" % get_or_none(request.GET, "value"))
+    print("input_address: %s" % get_or_none(request.GET, "input_address"))
+    print("confirmations: %s" % get_or_none(request.GET, "confirmations"))
+    print("transaction_hash: %s" % get_or_none(request.GET, "transaction_hash"))
+    print("destination_address: %s" % get_or_none(request.GET, "destination_address"))
+    print("input_transaction_hash: %s" % get_or_none(request.GET, "input_transaction_hash"))
     return HttpResponse("*ok*")
