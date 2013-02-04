13db29f877046c180a0cdde7442c712757af948b
tonylampada <tonylampada@gmail.com>
2013-02-04 19:18:57 -0200

    testing callback...

--- djangoproject/core/views/bitcoin_views.py
+++ djangoproject/core/views/bitcoin_views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from core.utils.frespo_utils import dictOrEmpty
+import logging
+
+logger = logging.getLogger(__name__)
 
 def bitcoinIPN(request):
-    print('----- bitcoinIPN ------')
-    print("value: %s" % dictOrEmpty(request.GET, "value"))
-    print("input_address: %s" % dictOrEmpty(request.GET, "input_address"))
-    print("confirmations: %s" % dictOrEmpty(request.GET, "confirmations"))
-    print("transaction_hash: %s" % dictOrEmpty(request.GET, "transaction_hash"))
-    print("destination_address: %s" % dictOrEmpty(request.GET, "destination_address"))
-    print("input_transaction_hash: %s" % dictOrEmpty(request.GET, "input_transaction_hash"))
+    logger.info('----- bitcoinIPN ------')
+    logger.info("value: %s" % dictOrEmpty(request.GET, "value"))
+    logger.info("input_address: %s" % dictOrEmpty(request.GET, "input_address"))
+    logger.info("confirmations: %s" % dictOrEmpty(request.GET, "confirmations"))
+    logger.info("transaction_hash: %s" % dictOrEmpty(request.GET, "transaction_hash"))
+    logger.info("destination_address: %s" % dictOrEmpty(request.GET, "destination_address"))
+    logger.info("input_transaction_hash: %s" % dictOrEmpty(request.GET, "input_transaction_hash"))
+    logger.info('----- bitcoinIPN end ------')
     return HttpResponse("*ok*")
