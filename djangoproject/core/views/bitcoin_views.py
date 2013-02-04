6e810e0b265427104e1a7141e6f85ea95e30d671
tonylampada <tonylampada@gmail.com>
2013-02-04 19:15:29 -0200

    still testing callback

--- djangoproject/core/views/bitcoin_views.py
+++ djangoproject/core/views/bitcoin_views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
-from core.utils.frespo_utils import get_or_none
+from core.utils.frespo_utils import dictOrEmpty
 
 def bitcoinIPN(request):
     print('----- bitcoinIPN ------')
-    print("value: %s" % get_or_none(request.GET, "value"))
-    print("input_address: %s" % get_or_none(request.GET, "input_address"))
-    print("confirmations: %s" % get_or_none(request.GET, "confirmations"))
-    print("transaction_hash: %s" % get_or_none(request.GET, "transaction_hash"))
-    print("destination_address: %s" % get_or_none(request.GET, "destination_address"))
-    print("input_transaction_hash: %s" % get_or_none(request.GET, "input_transaction_hash"))
+    print("value: %s" % dictOrEmpty(request.GET, "value"))
+    print("input_address: %s" % dictOrEmpty(request.GET, "input_address"))
+    print("confirmations: %s" % dictOrEmpty(request.GET, "confirmations"))
+    print("transaction_hash: %s" % dictOrEmpty(request.GET, "transaction_hash"))
+    print("destination_address: %s" % dictOrEmpty(request.GET, "destination_address"))
+    print("input_transaction_hash: %s" % dictOrEmpty(request.GET, "input_transaction_hash"))
     return HttpResponse("*ok*")
