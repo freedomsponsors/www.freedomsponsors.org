5a8833ff52c79240aa614f97b375fd2abd9face2
tonylampada <tonylampada@gmail.com>
2013-02-14 01:41:17 -0200

    created job for async bitcoin transaction processing

---
+++ djangoproject/core/management/commands/bitcoin_jobs.py
@@ -0,0 +1,16 @@
+from django.core.management.base import NoArgsCommand
+from optparse import make_option
+from core.services import bitcoin_frespo_services
+
+__author__ = 'tony'
+
+class Command(NoArgsCommand):
+
+    help = "Asynchronous Bitcoin transaction processing"
+
+    option_list = NoArgsCommand.option_list + (
+        make_option('--verbose', action='store_true'),
+    )
+
+    def handle_noargs(self, **options):
+        bitcoin_frespo_services.bitcoin_active_receive_confirmation()
