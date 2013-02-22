06d0c2d1486d10cb2db55cbe4d09bef6a7b23a15
tonylampada <tonylampada@gmail.com>
2013-02-22 17:31:46 -0300

    removing splinter tests from Travis

--- djangoproject/coverage.sh
+++ djangoproject/coverage.sh
@@ -2,7 +2,9 @@
 # Coverage utility script
 set -e
 coverage -e
-coverage -x manage.py test core gh_frespo_integration core_splinter_tests
+# coverage -x manage.py test core gh_frespo_integration core_splinter_tests
+coverage -x manage.py test core core_splinter_tests
 coverage -r -m > report.xml
 rm -Rf coverage_html_report
-coverage html '--include=core/*,gh_frespo_integration/*,bitcoin_frespo/*' '--omit=core/migrations/*'
+# coverage html '--include=core/*,gh_frespo_integration/*,bitcoin_frespo/*' '--omit=core/migrations/*'
+coverage html '--include=core/*,bitcoin_frespo/*' '--omit=core/migrations/*'
