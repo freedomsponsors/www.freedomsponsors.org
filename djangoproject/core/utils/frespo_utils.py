be7680ef54c0727193f07cc03773e9e6a2076dac
Iuri de Silvio <iurisilvio@gmail.com>
2013-05-22 09:02:13 -0300

    Move frespo_utils.Struct to core_splinter_tests.tests.helpers.appdriver

--- djangoproject/core/utils/frespo_utils.py
+++ djangoproject/core/utils/frespo_utils.py
@@ -11,8 +11,3 @@ def get_or_none(model, **kwargs):
 #TODO: use this in a bunch of places (needed because sqlite doesn't store the .00 Decimal places - and this breaks a few tests)
 def twoplaces(dec):
     return dec.quantize(_TWOPLACES)
-
-class Struct:
-    def __init__(self, **entries): 
-        self.__dict__.update(entries)
-
