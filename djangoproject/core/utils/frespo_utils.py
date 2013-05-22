f82f074b2066edd36b7ac1fec47e0a26772f5bbf
Iuri de Silvio <iurisilvio@gmail.com>
2013-05-22 08:22:03 -0300

    Move frespo_utils.sendhtml_mail to core.services.mail_services

--- djangoproject/core/utils/frespo_utils.py
+++ djangoproject/core/utils/frespo_utils.py
@@ -1,11 +1,5 @@
 from urlparse import urlparse
 from decimal import Decimal
-from django.core.mail import EmailMultiAlternatives
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
-import logging
-
-logger = logging.getLogger(__name__)
 
 _TWOPLACES = Decimal(10) ** -2
 
@@ -54,17 +48,6 @@ def get_or_none(model, **kwargs):
         return model.objects.get(**kwargs)
     except model.DoesNotExist:
         return None
-
-def send_html_mail(subject, body_txt, body_html, from_email, to_addresses):
-    try:
-        for to_addr in to_addresses:
-            validate_email(to_addr)
-    except ValidationError:
-        logger.warn('Email not sent. Invalid email address in %s. subject = %s' % (to_addresses, subject))
-        return
-    msg = EmailMultiAlternatives(subject, body_txt, from_email, to_addresses)
-    msg.attach_alternative(body_html, "text/html")
-    msg.send()
 
 #TODO: use this in a bunch of places (needed because sqlite doesn't store the .00 Decimal places - and this breaks a few tests)
 def twoplaces(dec):
