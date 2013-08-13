f7c52e3aa6961455ba9e8b6bf5400618fb7cc86f
Rafael Santos <rafael.agt.santos@gmail.com>
2013-08-13 13:13:36 -0300

    Modificações na home, criação pagina da issue

--- djangoproject/sandbox/urls.py
+++ djangoproject/sandbox/urls.py
@@ -5,6 +5,6 @@ __author__ = 'tony'
 
 urlpatterns = patterns('sandbox.views',
     url(r'^$', direct_to_template, {'template': 'sandbox/home.html'}),
-    url(r'^issue$', direct_to_template, {'template': 'sandbox/issue.html'}),
+    url(r'^issue_page$', direct_to_template, {'template': 'sandbox/issue_page.html'}),
     # url(r'^home/$', 'home'),
 )
