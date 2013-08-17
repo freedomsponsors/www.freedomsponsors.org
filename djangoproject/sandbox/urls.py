c234f55fce4b808a652ea2696c3a5a3e90e08c9a
Tony Lampada <tonylampada@gmail.com>
2013-08-17 19:26:05 -0300

    Including modal

--- djangoproject/sandbox/urls.py
+++ djangoproject/sandbox/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('sandbox.views',
     url(r'^issue_page$', direct_to_template, {'template': 'sandbox/issue_page.html'}),
     url(r'^adropdown', direct_to_template, {'template': 'sandbox/adropdown.html'}),
     url(r'^atabnav', direct_to_template, {'template': 'sandbox/atabnav.html'}),
+    url(r'^amodal', direct_to_template, {'template': 'sandbox/amodal.html'}),
     # url(r'^home/$', 'home'),
 )
