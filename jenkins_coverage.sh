91c58e36281c48a9248930036842f14feffa0a93
Tony Lampada <tonylampada@gmail.com>
2013-12-27 13:01:13 -0200

    Have Jenkins publish HTML Coverage reports.

---
+++ jenkins_coverage.sh
@@ -0,0 +1,12 @@
+set -e
+python bootstrap
+source bin/activate
+cd djangoproject
+cp frespo/env_settings.py_template frespo/env_settings.py 
+
+coverage -e
+coverage -x manage.py test core gh_frespo_integration
+coverage -r -m > report.xml
+rm -Rf coverage_html_report
+coverage html '--include=core/*,gh_frespo_integration/*,bitcoin_frespo/*' '--omit=core/migrations/*'
+
