603eff406048674a8953f33a07fef5888e697154
Tony Lampada <tonylampada@gmail.com>
2014-02-07 22:03:59 -0200

    auto destroy test database

--- jenkins.sh
+++ jenkins.sh
@@ -2,4 +2,4 @@ python bootstrap
 source bin/activate
 cd djangoproject
 cp frespo/env_settings.py_template frespo/env_settings.py 
-./manage.py test core gh_frespo_integration
+./manage.py test core gh_frespo_integration --noinput
