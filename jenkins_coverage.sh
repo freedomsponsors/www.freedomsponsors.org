c3c090374e1dc3aebc71651599e0bec0c5248b4a
tonylampada <tonylampada@gmail.com>
2014-06-12 18:46:07 -0300

    fix coverage script

--- jenkins_coverage.sh
+++ jenkins_coverage.sh
@@ -5,7 +5,7 @@ cd djangoproject
 cp frespo/env_settings.py_template frespo/env_settings.py 
 
 coverage -e
-coverage -x manage.py test core gh_frespo_integration
+coverage -x manage.py test --noinput core gh_frespo_integration
 coverage -r -m > report.xml
 rm -Rf coverage_html_report
 coverage html '--include=core/*,gh_frespo_integration/*,bitcoin_frespo/*' '--omit=core/migrations/*'
