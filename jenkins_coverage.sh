bc533778eb34a5b43ec8af81fa6ba5151528b66d
tonylampada <tonylampada@gmail.com>
2014-07-05 22:22:48 -0300

    trying to improve coverage report

--- jenkins_coverage.sh
+++ jenkins_coverage.sh
@@ -7,6 +7,13 @@ cp frespo/env_settings.py_template frespo/env_settings.py
 coverage -e
 coverage -x manage.py test --noinput core gh_frespo_integration
 coverage -r -m > report.xml
-rm -Rf coverage_html_report
-coverage html '--include=core/*,gh_frespo_integration/*,bitcoin_frespo/*' '--omit=core/migrations/*'
+# rm -Rf coverage_html_report
+coverage xml '--include=core/*,gh_frespo_integration/*,bitcoin_frespo/*' '--omit=core/migrations/*'
 
+# coverage erase
+# coverage run --source='core' '--omit=core/tests/**,core/migrations/**' manage.py test --noinput core
+# coverage xml
+# cp -Rf * ../../
+
+# set -
+# deactivate || : # virtualenv
