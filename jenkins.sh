a11ec3436237df4e02f7756c0bd54f8cc15b2a04
tonylampada <tonylampada@gmail.com>
2014-07-06 10:40:00 -0300

    test jenkins emails

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
+exit 1
 python bootstrap
 source bin/activate
 cd djangoproject
 cp frespo/env_settings.py_template frespo/env_settings.py 
 ./manage.py test core gh_frespo_integration --noinput
 
-#teste
