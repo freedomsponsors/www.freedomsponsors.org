4500f76da585ed66c24b0388cfa36f7595a6819f
Kang Zhao <zhaokang.cn@gmail.com>
2014-07-07 22:40:11 +0800

    Partial fix of #128, /core/stats -> /stats

--- djangoproject/frespo/urls.py
+++ djangoproject/frespo/urls.py
@@ -23,6 +23,7 @@ if 'core' in settings.INSTALLED_APPS:
         url(r'^offer/', include('core.urls.offer_urls')),
         url(r'^solution/', include('core.urls.solution_urls')),
         url(r'^search/', 'core.views.issue_views.listIssues'),
+        url(r'^stats/',  'core.views.main_views.stats'),
         url(r'^github-hook/', include('core.urls.github_hook_urls')),
         url(r'^feedback', include('core.urls.feedback_urls')),
         url(r'^user/', include('core.urls.user_urls')),
