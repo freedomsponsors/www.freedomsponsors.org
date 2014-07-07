00022c7dea0a9848c0c21fc52d2ff1132b30378f
Kang Zhao <zhaokang.cn@gmail.com>
2014-07-07 23:39:37 +0800

    polish codes

--- djangoproject/core/urls/__init__.py
+++ djangoproject/core/urls/__init__.py
@@ -5,34 +5,30 @@ from django.views.generic.simple import redirect_to, direct_to_template
 
 urlpatterns = patterns('core.views.main_views',
     url(r'^$', 'home'),
-    url(r'^home/$', RedirectView.as_view(url='/', permanent=True)),
     url(r'^admail/$', 'admail'),
     url(r'^mailtest/$', 'mailtest'),
     url(r'^about/$', redirect_to, {'url': 'http://blog.freedomsponsors.org/about/'}),
-    url(r'^faq/$', RedirectView.as_view(url='/faq', permanent=True)),
     url(r'^dev/$', redirect_to, {'url': 'http://blog.freedomsponsors.org/developers/'}),
 )
 
-# below url redirections ensure existing/external links to /core/jslic still work
+# 301 redirects
 urlpatterns += patterns('',
-    url(r'^jslic$',  RedirectView.as_view(url='/jslic',  permanent=True, query_string=True)),
-)
+    url(r'^home/$',    RedirectView.as_view(url='/',         permanent=True)),
+    url(r'^faq$',      RedirectView.as_view(url='/faq',      permanent=True)),
+    url(r'^jslic$',    RedirectView.as_view(url='/jslic',    permanent=True)),
+    url(r'^stats$',    RedirectView.as_view(url='/stats',    permanent=True)),
+    url(r'^feedback$', RedirectView.as_view(url='/feedback', permanent=True)),
 
-# below url redirections ensure existing/external links to /core/stats still work
-urlpatterns += patterns('',
-    url(r'^stats/$',  RedirectView.as_view(url='/stats/',        permanent=True, query_string=True)),
-)
-
-# below url redirections ensure existing/external links to /core/issue/* urls still work
-urlpatterns += patterns('',
     url(r'^issue/$',              RedirectView.as_view(url='/search/',        permanent=True, query_string=True)),
     url(r'^issue/(?P<temp>.*)$',  RedirectView.as_view(url='/issue/%(temp)s', permanent=True, query_string=True)),
-)
 
-urlpatterns += patterns('',
-    url(r'^project/$', RedirectView.as_view(url='/project/', permanent=True)),
-    url(r'^project/(?P<project_id>\d+)/$', RedirectView.as_view(url='/project/%(project_id)s/', permanent=True)),
+    url(r'^project/$',                         RedirectView.as_view(url='/project/', permanent=True)),
+    url(r'^project/(?P<project_id>\d+)/$',     RedirectView.as_view(url='/project/%(project_id)s/', permanent=True)),
     url(r'^project/(?P<project_id>\d+)/edit$', RedirectView.as_view(url='/project/%(project_id)s/edit', permanent=True)),
+
+    url(r'^user/$',                                   RedirectView.as_view(url='/user/', permanent=True)),
+    url(r'^user/(?P<user_id>\d+)/$',                  RedirectView.as_view(url='/user/%(user_id)s/', permanent=True)),
+    url(r'^user/(?P<user_id>\d+)/(?P<user_slug>.*)$', RedirectView.as_view(url='/user/%(user_id)s/%(user_slug)s', permanent=True)),
 )
 
 urlpatterns += patterns('core.views.paypal_views',
@@ -45,12 +41,6 @@ urlpatterns += patterns('core.views.bitcoin_views',
     url(r'^bitcoin/'+settings.BITCOIN_IPNNOTIFY_URL_TOKEN+'$', 'bitcoinIPN'),
 )
 
-urlpatterns += patterns('',
-    url(r'^user/$', RedirectView.as_view(url='/user/', permanent=True)),
-    url(r'^user/(?P<user_id>\d+)/$', RedirectView.as_view(url='/user/%(user_id)s/', permanent=True)),
-    url(r'^user/(?P<user_id>\d+)/(?P<user_slug>.*)$', RedirectView.as_view(url='/user/%(user_id)s/%(user_slug)s', permanent=True)),
-)
-
 urlpatterns += patterns('core.views.json_views',
     url(r'^json/project$', 'project'),
     url(r'^json/by_issue_url$', 'by_issue_url'),
@@ -60,8 +50,4 @@ urlpatterns += patterns('core.views.json_views',
     url(r'^json/remove_tag', 'remove_tag'),
     url(r'^json/latest_activity', 'latest_activity'),
     url(r'^json/toggle_watch', 'toggle_watch'),
-)
-
-urlpatterns += patterns('',
-    url(r'^feedback$', RedirectView.as_view(url='/feedback', permanent=True)),
 )
