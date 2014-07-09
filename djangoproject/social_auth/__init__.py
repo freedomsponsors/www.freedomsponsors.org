1add97c4b0b01a5a5ad5c55fa3157cb1df0dcb98
tonylampada <tonylampada@gmail.com>
2014-07-09 09:59:40 -0300

    this code shouldnt have been commited

--- djangoproject/social_auth/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Django-social-auth application, allows OpenId or OAuth user
-registration/authentication just adding a few configurations.
-"""
-version = (0, 7, 20)
-__version__ = '.'.join(map(str, version))
