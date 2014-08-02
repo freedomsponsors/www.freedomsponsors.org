2ee774b47f0183f7721877e181953a8cbb2d302e
tonylampada <tonylampada@gmail.com>
2014-08-02 12:58:47 -0300

    sandbox need no database

--- djangoproject/frespo/settings_sandbox.py
+++ djangoproject/frespo/settings_sandbox.py
@@ -10,17 +10,6 @@ SITE_HOST = 'localhost:8080'
 SITE_NAME = 'FreedomSponsors'
 SITE_HOME = SITE_PROTOCOL+'://'+SITE_HOST
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
