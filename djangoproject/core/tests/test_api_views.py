ed562d683dc55f94848fa6792c96ebb2e9def365
CecileSun <cecile829@gmail.com>
2015-02-26 20:02:02 +0900

    add test for the if_not_project case

--- djangoproject/core/tests/test_api_views.py
+++ djangoproject/core/tests/test_api_views.py
@@ -2,7 +2,7 @@ import json
 from django.test import TestCase
 from helpers import test_data
 from django.test.client import Client
-
+from core.models import Project
 
 class TestGetProjectData(TestCase):
     def setUp(self):
@@ -22,3 +22,15 @@ class TestGetProjectData(TestCase):
         self.assertEqual(1, project['stats']['issues_open'])
         self.assertEqual(100, project['stats']['percent_issues_open'])
         self.assertEqual(10.0, project['stats']['usd_open'])
+
+	responseNG1 = self.client.get('/api/project/-1')
+	self.assertEqual(responseNG1.status_code, 404)
+	self.assertEqual(json.loads(responseNG1.content)['error'], 'Project not found')
+
+	#responseNG2 = self.client.get('/api/project/100000000)
+	#self.assertEqual(responseNG2.status_code, 404)
+	#self.assertEqual(json.loads(responseNG2.content)['error'], 'Project not found')
+
+	#responseNG3 = self.client.get('/api/project/A)
+	#self.assertEqual(responseNG3.status_code, 404)
+	#self.assertEqual(json.loads(responseNG3.content)['error'], 'Project not found')
